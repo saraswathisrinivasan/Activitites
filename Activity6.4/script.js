39bf6ca9a0f1117156bf6bf6d0260780374f973b
saraswathisrinivasan <41221196+saraswathisrinivasan@users.noreply.github.com>
2023-01-29 20:34:25 +0530

    Add files via upload

--- Activity6.4/script.js
+++ Activity6.4/script.js
@@ -1,1 +1,43 @@
-
+class UberCharge
+{
+   
+    constructor(baseFare,costPerMint,timeInRide,costPerMile,rideDistance,bookingFee)
+    {
+        this.baseFare=baseFare
+        this.costPerMint=costPerMint
+        this.timeInRide=timeInRide
+        this.costPerMile=costPerMile
+        this.rideDistance=rideDistance
+        this.bookingFee=bookingFee
+    }
+    getBaseFare()
+    {
+        return this.baseFare
+    }
+    getCostPerMint()
+    {
+        return this.costPerMint
+    }
+    getTimeInRide()
+    {
+        return this.timeInRide
+    }
+    getCostPerMile()
+    {
+        return this.costPerMile
+    }
+    getRideDistance()
+    {
+        return this.rideDistance
+    }
+    getBookingFee()
+    {
+        return this.bookingFee
+    }
+    calculateUberCharges()
+    {
+        return ("Your Charge = ",this.baseFare + (this.costPerMint * this.timeInRide) + (this.costPerMile * this.rideDistance) + this.bookingFee)
+    }
+}
+const charge=new UberCharge(300,2,2,10,10,300)
+console.log(charge.calculateUberCharges())
